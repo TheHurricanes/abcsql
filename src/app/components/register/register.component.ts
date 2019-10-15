ac856c5a4d7a4e9b955672513c1236b27d46f670
Abraham Hernandez <abraham@abranhe.com>
2019-10-15 03:58:44 -0400

    cleaning up

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,54 +1,56 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormControl, FormGroup, Validators }  from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { UserService } from 'src/app/services/user.service';
 import { NgFlashMessageService } from 'ng-flash-messages';
-
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent implements OnInit {
-  registerForm:FormGroup;
-  userOb:{};
-  constructor(private fb :FormBuilder, private userService:UserService,
-    private flashService:NgFlashMessageService) { }
+  registerForm: FormGroup;
+  userOb: {};
+  constructor(private fb: FormBuilder, private userService: UserService,
+    private flashService: NgFlashMessageService) { }
 
   ngOnInit() {
     this.registerForm = new FormGroup({
       name: new FormControl('', Validators.required),
-      username: new FormControl('', Validators.required),  
-      email: new FormControl('',[Validators.email,Validators.required]),
-      password: new FormControl('',Validators.required)
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', Validators.required)
     });
   }
-  onRegisterSubmit(form){
+
+  onRegisterSubmit(form) {
     this.userOb = {
-      name:form.name,
-      username:form.username,
-      email:form.email,
-      password:form.password
+      name: form.name,
+      username: form.username,
+      email: form.email,
+      password: form.password
     }
-      this.registerForm.reset();
-      this.userService.registerService(this.userOb).subscribe( data =>
-      {
-        if(data['success']) {
-            this.flashService.showFlashMessage({
-              messages: [data['msg']], 
-              dismissible: true, 
-              timeout: 3000,
-              type: 'success'
-            });
-        } else {
-            this.flashService.showFlashMessage({
-              messages: [data['msg']], 
-              dismissible: true, 
-              timeout: 3000,
-              type: 'danger'
-            });
-        }
+
+    this.registerForm.reset();
+
+    this.userService.registerService(this.userOb).subscribe(data => {
+      if (data['success']) {
+        this.flashService.showFlashMessage({
+          messages: [data['msg']],
+          dismissible: true,
+          timeout: 3000,
+          type: 'success'
+        });
+      } else {
+        this.flashService.showFlashMessage({
+          messages: [data['msg']],
+          dismissible: true,
+          timeout: 3000,
+          type: 'danger'
+        });
       }
-    );    
+    }
+    );
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://ec2-18-208-129-70.compute-1.amazonaws.com'
+  url: 'https://abcsql.now.sh'
 };
 
 /*
